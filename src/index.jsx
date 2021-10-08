aa9191d13d68bb621f49b21ff4fa9b024861f5c6
Ghanshyam Hirani <87382991+Trupti7291@users.noreply.github.com>
2021-10-09 00:15:49 +0200

    Update index.jsx
    changed the message for output

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,7 @@ class MyFlixApplication extends React.Component {
     render() {
         return (
             <div className="my-flix">
-                <div> Hello World </div>
+                <div> Hello World! </div>
             </div>
         );
     }
