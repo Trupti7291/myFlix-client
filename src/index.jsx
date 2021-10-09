fd51e9ff37c62ea030f5a1879cef8cf0a026f696
Ghanshyam Hirani <87382991+Trupti7291@users.noreply.github.com>
2021-10-09 11:39:41 +0200

    Update index.jsx
    updated the import react-dom from React-Dom

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ReactDOM } from "react";
+import ReactDOM from "react-dom";
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
