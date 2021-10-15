99615d84445ead13bcebcc8a17de35a2ed148c5c
Ghanshyam Hirani <87382991+Trupti7291@users.noreply.github.com>
2021-10-15 16:12:59 +0200

    Create button.jsx
    Created and Initiated Button,jsx

---
+++ src/components/button/button.jsx
@@ -0,0 +1,8 @@
+import Button from "react-bootstrap/Button";
+import './button.scss';
+
+export function button({ label }) {
+    <Button variant="primary" type="submit" onClick={handleSubmit}>
+        Submit
+    </Button>
+}
