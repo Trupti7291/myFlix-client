7a23b33a6a2a8aec8ab670cac3f82ed051228ffa
Ghanshyam Hirani <87382991+Trupti7291@users.noreply.github.com>
2021-10-15 08:11:00 +0200

    Update movie-card.jsx
    set the static propTypes property on MovieCard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import propTypes, { func } from 'prop-types';
 
 export class MovieCard extends React.Component {
     render() {
@@ -6,3 +7,10 @@ export class MovieCard extends React.Component {
         return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title} </div>;
     }
 }
+
+MovieCard.propTypes = {
+    movie: propTypes.shape({
+        Title: propTypes.string
+    }).isRequired,
+    onMovieClick: propTypes.func.isRequired
+};
