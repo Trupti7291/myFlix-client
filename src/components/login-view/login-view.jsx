5b70b9ac74ccd906ec1a88a6961e4faa630ea0cb
Ghanshyam Hirani <87382991+Trupti7291@users.noreply.github.com>
2021-10-15 16:52:08 +0200

    Update login-view.jsx
    Added React Bootstrap Components & React Bootstrap Grid System

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form, Button } from 'react-bootstrap';
 
 export function LoginView(props) {
     const [username, setUsername] = useState("");
@@ -11,15 +12,18 @@ export function LoginView(props) {
         props.onLoggedIn(username);
     }
     return (
-        <form>
-            <label>
-                Username:
-                <input type="text" value={this.state.username} onChange={this.onUsernameChange} />
-            </label>
-            <label>
-                <input type="password" value={this.state.password} onChange={this.onPasswordChange} />
-            </label>
-            <button type="button" onClick={this.handleSubmit}> Submit </button>
-        </form>
+        <Form>
+            <Form.Group controlId="formUsername">
+                <Form.Label>Username: </Form.Label>
+                <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+            </Form.Group>
+            <Form.Group controlId="formPassword">
+                <Form.Label>Password: </Form.Label>
+                <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={handleSubmit}>
+                Submit
+            </Button>
+        </Form>
     );
 }
