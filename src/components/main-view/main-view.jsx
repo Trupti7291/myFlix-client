720d1ffbc29ee8d40689dbe2b73434fdb33357e6
Ghanshyam Hirani <87382991+Trupti7291@users.noreply.github.com>
2021-10-15 07:40:27 +0200

    Update main-view.jsx
    empty the movie: [array]

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,11 +8,7 @@ export class MainView extends React.Component {
     constructor() {
         super();
         this.state = {
-            movies: [
-                { _id: 1, Title: " The Notebook ", Description: " A poor yet passionate young man falls in love with a rich young woman, giving her a sense of freedom, but they are soon separated because of their social differences. ", Imagepath: " https://upload.wikimedia.org/wikipedia/en/8/86/Posternotebook.jpg " },
-                { _id: 2, Title: " Titanic ", Description: "A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.", Imagepath: "https://upload.wikimedia.org/wikipedia/en/1/19/Titanic_%28Official_Film_Poster%29.png" },
-                { _id: 3, Title: "Jurassic Park", "Description": "A pragmatic paleontologist visiting an almost complete theme park is tasked with protecting a couple of kids after a power failure causes the park's cloned dinosaurs to run loose.", Imagepath: "https://images-na.ssl-images-amazon.com/images/I/8142L+TQEyL.jpg" }
-            ],
+            movies: [],
             selectedMovie: null
         };
     }
