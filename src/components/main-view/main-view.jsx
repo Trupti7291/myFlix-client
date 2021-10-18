3e4be258b2bc4c6e29e24a7522aa140fb93986ac
Ghanshyam Hirani <87382991+Trupti7291@users.noreply.github.com>
2021-10-18 23:20:07 +0200

    Update main-view.jsx
    added Registeraion view that allowes new user to register

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from '../registration-view/registration-view';
 import { Container, Row, Col } from 'react-bootstrap';
 
 export class MainView extends React.Component {
@@ -16,7 +17,7 @@ export class MainView extends React.Component {
         };
     }
 
-    componentDidMount() {
+    /* componentDidMount() {
         axios.get('https://my-flixapp.herokuapp.com/movies')
             .then(response => {
                 this.setState({
@@ -26,6 +27,16 @@ export class MainView extends React.Component {
             .catch(error => {
                 console.log(error);
             });
+    } */
+
+    componentDidMount() {
+        let accessToken = localStorage.getItem('token');
+        if (accessToken !== null) {
+            this.setState({
+                user: localStorage.getItem('user')
+            });
+            this.getMovies(accessToken);
+        }
     }
 
     /*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie*/
@@ -35,21 +46,45 @@ export class MainView extends React.Component {
         });
     }
 
-    /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
-    onLoggedIn(user) {
+    onRegistration(register) {
+        console.log(user);
         this.setState({
-            user
+            register
         });
     }
 
+    /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
+    onLoggedIn(authData) {
+        console.log(authData);
+        this.setState({
+            user: authData.user.Username
+        });
+        localStorage.setItem('token', authData.token);
+        localStorage.setItem('user', authData.user.Username);
+        this.getMovies(authData.token);
+    }
+
+    getMovies(token) {
+        axios.get('https://my-flixapp.herokuapp.com/movies', {
+            headers: { Authorization: `Bearer ${token}` }
+        })
+            .then(response => {
+                // Assign the result to the state
+                this.setState({
+                    movies: response.data
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
     render() {
-        const { movies, selectedMovie, user } = this.state;
+        const { movies, selectedMovie, user, register } = this.state;
 
         /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
         if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
-
-        if (selectedMovie)
-            return <MovieView movie={selectedMovie} />;
+        if (!register) return <RegistrationView onRegistration={register => this.onRegistration(register)} />
 
         if (movies.length === 0) return <div className="main-view" />;
 
