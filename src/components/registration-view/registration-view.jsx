64ccfd5581896caf00529ea946cdd4a304282d79
Ghanshyam Hirani <87382991+Trupti7291@users.noreply.github.com>
2021-10-21 07:23:39 +0200

    Update registration-view.jsx
    Removed Navbar component

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Button, Container, Row, Col, Navbar } from "react-bootstrap";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
 
 export function RegistrationView(props) {
     const [username, setUsername] = useState("");
@@ -17,27 +17,25 @@ export function RegistrationView(props) {
         <Container>
             <Row className="main-view justify-content-md-center">
                 <Col md={8}>
-                    <Navbar bg="dodgerblue">
-                        <Form>
-                            <Form.Group controlId="formUsername">
-                                <Form.Label>Username: </Form.Label>
-                                <Form.Control type="text" onChange={e => setUsername(e.target.value)} required placeholder="Enter Username" />
-                            </Form.Group>
-                            <Form.Group controlId="formPassword">
-                                <Form.Label>Password: </Form.Label>
-                                <Form.Control type="password" onChange={e => setPassword(e.target.value)} minLength="6" placeholder="Your password must contain min 8 characters" />
-                            </Form.Group>
-                            <Form.Group controlId="formEmail">
-                                <Form.Label>Email: </Form.Label>
-                                <Form.Control type="email" onChange={e => setEmail(e.target.value)} required placeholder="Enter your Email address" />
-                            </Form.Group>
-                            <Form.Group controlId="formBirthdate">
-                                <Form.Label>Birthdate: </Form.Label>
-                                <Form.Control type="date" onChange={e => setBirthdate(e.target.value)} required placeholder="yyyy.mm.dd" />
-                            </Form.Group>
-                            <Button variant="primary" type="submit" onClick={handleSubmit}> Sign up </Button>
-                        </Form>
-                    </Navbar>
+                    <Form>
+                        <Form.Group controlId="formUsername">
+                            <Form.Label>Username: </Form.Label>
+                            <Form.Control type="text" onChange={e => setUsername(e.target.value)} required placeholder="Enter Username" />
+                        </Form.Group>
+                        <Form.Group controlId="formPassword">
+                            <Form.Label>Password: </Form.Label>
+                            <Form.Control type="password" onChange={e => setPassword(e.target.value)} minLength="6" placeholder="Your password must contain min 8 characters" />
+                        </Form.Group>
+                        <Form.Group controlId="formEmail">
+                            <Form.Label>Email: </Form.Label>
+                            <Form.Control type="email" onChange={e => setEmail(e.target.value)} required placeholder="Enter your Email address" />
+                        </Form.Group>
+                        <Form.Group controlId="formBirthdate">
+                            <Form.Label>Birthdate: </Form.Label>
+                            <Form.Control type="date" onChange={e => setBirthdate(e.target.value)} required placeholder="yyyy.mm.dd" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit" onClick={handleSubmit}> Sign up </Button>
+                    </Form>
                 </Col>
             </Row>
         </Container>
