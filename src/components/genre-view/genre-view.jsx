49bcb4940bc4bab4d64f4da3ebb8d7e86aaed82f
Ghanshyam Hirani <87382991+Trupti7291@users.noreply.github.com>
2021-10-22 10:01:47 +0200

    Create genre-view.jsx
    created and initiated genre view

---
+++ src/components/genre-view/genre-view.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+export class Genreview extends React.Component {
+    render() {
+        const { movie, onBackClick } = this.props;
+
+        return (
+            <div className="genre-view">
+
+                <div className="genre-name">
+                    <span className="value">{movie.Genre.Name}</span>
+                </div>
+
+                <div className="director-discription">
+                    <span className="value">{movie.Genre.Description}</span>
+                </div>
+
+                <Button variant="primary" onClick={() => { onBackClick(null); }}>Back</Button>
+
+            </div>
+        );
+    }
+}
