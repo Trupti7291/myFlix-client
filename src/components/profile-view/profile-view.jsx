c7c3b2272f23e1dcb5f46ccec1a90bafb1901af4
Ghanshyam Hirani <87382991+Trupti7291@users.noreply.github.com>
2021-10-22 10:01:00 +0200

    Create profile-view.jsx
    created and initiated profile view using raect bootstrap components  and grid

---
+++ src/components/profile-view/profile-view.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import { Container, Card, Row, Col, Figure } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import UserInfo from "./user-info";
+import FavouriteMovies from "./favourite-movies";
+import UpdateUser from "./update-user";
+import "./profile-view.scss";
+
+export function ProfileView({ movies, onUpdatedUserInfo }) {
+    const [user, setUser] = useState("");
+    const getUser = () => { };
+    const handleSubmit = (e) => { };
+    const removeFav = (id) => { };
+    const handleUpdate = (e) => { };
+    useEffect(() => { }, []);
+}
+return (
+    <Container>
+        <Container>
+            <Row>
+                <Col sm={12} sm={4}>
+                    <Card>
+                        <Card.Body>
+                            <UserInfo name={user.Username} email={user.Email} />
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col sm={12} sm={8}>
+                    <Card>
+                        <Card.Body>
+                            <FavouriteMovies favoriteMovieList={favoriteMovieList} />
+                        </Card.Body>
+                    </Card>
+
+                </Col>
+            </Row>
+            <UpdateUser handleSubmit={handleSubmit} handleUpdate={handleUpdate} />
+        </Container>
+    </Container>
+);
