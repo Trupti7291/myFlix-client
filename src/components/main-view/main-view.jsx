68eb978e436a43f6f411400c3b9c310a7c27a8cf
Ghanshyam Hirani <87382991+Trupti7291@users.noreply.github.com>
2021-10-29 10:54:36 +0200

    Update main-view.jsx
    imported profile view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,6 +8,7 @@ import { MovieView } from '../movie-view/movie-view';
 import { DirectorView } from '../director-view/director-view';
 import { Genreview } from '../genre-view/genre-view';
 import { RegistrationView } from '../registration-view/registration-view';
+import { ProfileView } from '../profile-view/profile-view';
 
 import { Container, Row, Col } from 'react-bootstrap';
 
