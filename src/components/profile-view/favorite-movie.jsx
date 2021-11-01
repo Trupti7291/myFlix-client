691fe66d819ed20c80ba2632d652a0c3d96682d8
Ghanshyam Hirani <87382991+Trupti7291@users.noreply.github.com>
2021-11-01 21:37:51 +0100

    Create favorite-movie.jsx
    recreated file

---
+++ src/components/profile-view/favorite-movie.jsx
@@ -0,0 +1,51 @@
+import axios from "axios";
+import React from "react";
+import { Button, Container, Row, Col, Figure, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+function FavouriteMovies({ favoriteMovieList }) {
+    const removeFav = (id) => {
+        let token = localStorage.getItem('token');
+        let url = `https://my-flixapp.herokuapp.com/users/${localStorage.getItem('user')}/movies/${id}`;
+        axios.delete(url, {
+            headers: { Authorization: `Bearer ${token}` },
+        })
+    }
+
+    return (
+        <Container>
+            <Card>
+                <Card.Body>
+                    <Row>
+                        <Col
+                            sm={12}> <h2> Favourite Movies </h2>
+                        </Col>
+                    </Row>
+                    <Row>
+                        {favoriteMovieList.map((ImagePath, Title, _id) => {
+                            return (
+                                <Col xs={12} md={6} lg={3} key={_id}>
+                                    <Figure>
+                                        <Link to={`/movies/${movie._id}`}>
+                                            <Figure.Image
+                                                src={ImagePath}
+                                                alt={Title}
+                                            />
+                                            <Figure.Caption>
+                                                Title
+                                            </Figure.Caption>
+                                        </Link>
+                                    </Figure>
+                                    <Button variant="secondary" onClick={() => removeFav(movies._id)}> Remove from the list </Button>
+                                </Col>
+                            )
+                        })}
+                    </Row>
+                </Card.Body>
+            </Card>
+        </Container>
+    );
+}
+
+export default FavouriteMovies
