d53c367038c0cea287ccada36f678366a2e8ba21
Ghanshyam Hirani <87382991+Trupti7291@users.noreply.github.com>
2021-11-01 15:10:37 +0100

    Update director-view.jsx
    coded with mcv

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,28 +1,24 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import './director-view.scss';
 
 
 export class DirectorView extends React.Component {
     render() {
-        const { movie, onBackClick } = this.props;
+        const { director, movie, onBackClick } = this.props;
 
         return (
             <div className="director-view">
+                <Card>
+                    <Card.Body>
 
-                <div className="director-name">
-                    <span className="value">{movie.Director.Name}</span>
-                </div>
-
-                <div className="director-bio">
-                    <span className="value">{movie.Director.Bio}</span>
-                </div>
-
-                <div className="director-birthdate">
-                    <span className="value">{movie.Director.Birthdate}</span>
-                </div>
-                <button onClick={() => { onBackClick(null); }}> Back </button>
+                        <Card.Title className="director-name">{director.Name}</Card.Title>
+                        <Card.Text> {director.Bio}</Card.Text>
+                        <Card.Text> {director.Birthdate} </Card.Text>
+                        <button onClick={() => { onBackClick(null); }}> Back </button>
+                    </Card.Body>
+                </Card>
 
                 {/* <Button variant="primary" onClick={() => { onBackClick(null); }}>Back</Button> */}
 
