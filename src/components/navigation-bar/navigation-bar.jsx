5459ee684930b66044cea6105ad2674ddb7d076d
Ghanshyam Hirani <87382991+Trupti7291@users.noreply.github.com>
2021-11-03 13:09:11 +0100

    Update navigation-bar.jsx
    updated link to every page

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -4,6 +4,8 @@ import { Navbar, NavDropdown, Nav, Container } from "react-bootstrap";
 
 import { Link } from "react-router-dom";
 
+import "./navigation-bar.scss";
+
 export class NavigationBar extends React.Component {
     constructor() {
         super();
@@ -23,30 +25,22 @@ export class NavigationBar extends React.Component {
 
         return (
             <Container>
-                <Navbar collapseOnSelect fixed="top">
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar bg="light" collapseOnSelect fixed='top' expand="lg" variant="light" >
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav" />
-
                     <Container>
-                        <Navbar.Brand as={Link} to={movies} className="link-text"> </Navbar.Brand>
-                        <Nav className="me-auto">
-                            <NavDropdown title="MENU" id="basic-nav-dropdown">
-                                <NavDropdown.Item as={Link} to={movies} className="link-text">
-                                    Movies
-                                </NavDropdown.Item>
-                                <NavDropdown.Item as={Link} to={profile} className="link-text">
-                                    Profile
-                                </NavDropdown.Item>
-
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item to={"/"} onClick={this.onLoggedOut}>
-                                    Log Out
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
+                        <Nav.Link as={Link} to={movies} className="link-text">
+                            Movies
+                        </Nav.Link>
+                        <Nav.Link as={Link} to={profile} className="link-text">
+                            Profile
+                        </Nav.Link>
+                        <Nav.Link to={'/'} onClick={this.onLoggedOut}>
+                            Log Out
+                        </Nav.Link>
                     </Container>
-                </Navbar>
-            </Container>
+                </Navbar >
+            </Container >
         );
     }
 }
