9897a5e757ffe652770c3e60f13be6b6cc4ca7af
Ghanshyam Hirani <87382991+Trupti7291@users.noreply.github.com>
2021-11-10 11:47:46 +0100

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -33,9 +33,7 @@ export function LoginView(props) {
                         <Form.Label>Password: </Form.Label>
                         <Form.Control type="password" onChange={e => setPassword(e.target.value)} required placeholder="Enter Password" />
                     </Form.Group>
-                    <Button variant="primary" type="submit" onClick={handleSubmit}>
-                        Submit
-                    </Button>
+                    <Button variant="primary" type="submit" onClick={handleSubmit}> Log in </Button>
                 </Form>
             </Col>
         </Row>
