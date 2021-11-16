4d97ed89a6ee1eb2c753be8af8971d66b0b6fe46
Ghanshyam Hirani <87382991+Trupti7291@users.noreply.github.com>
2021-11-16 14:40:12 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Container from 'react-bootstrap/Container';
+<<<<<<< HEAD
 import { MainView } from './components/main-view/main-view';
+=======
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import moviesApp from './reducers/reducers';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+>>>>>>> myFlix-client
 
-// Import statement to indicate that you need to bundle `./index.scss`
+import MainView from './components/main-view/main-view';
+
+
+// Import statement to indicate that we need to bundle `./index.scss`
 import './index.scss';
+
+const store = createStore(moviesApp, devToolsEnhancer());
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
     render() {
         return (
+<<<<<<< HEAD
             <container>
                 <MainView />
             </container>
+=======
+            <Provider store={store}>
+                <Container>
+                    <MainView />
+                </Container>
+            </Provider>
+>>>>>>> myFlix-client
         );
     }
 }
 
-// Finds the root of your app
+// Find the root of our app
 const container = document.getElementsByClassName('app-container')[0];
 
-// Tells React to render your app in the root DOM element
+// Tell React to render our app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
