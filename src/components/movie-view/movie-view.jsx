f8d8310f8a2a1e1adbfb50ff35d6da8048f04f60
Ghanshyam Hirani <87382991+Trupti7291@users.noreply.github.com>
2021-11-16 14:38:27 +0100

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,36 +1,64 @@
-import React from 'react';
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 export class MovieView extends React.Component {
+    addFavoriteMovie(event, movie) {
+        const token = localStorage.getItem("token");
+        const username = localStorage.getItem("user");
+        console.log("username", username);
 
-    keypressCallback(event) {
-        console.log(event.key);
+        axios
+            .post(
+                `https://my-flixapp.herokuapp.com/users/${username}/movies/${movie._id}`,
+                {},
+                {
+                    headers: { Authorization: `Bearer ${token}` },
+                    "Content-Type": "application/json",
+                }
+            )
+            .then((response) => {
+                console.log("response", response);
+                alert(`Added to Favorites`);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
-
-    componentDidMount() {
-        document.addEventListener('keypress', this.keypressCallback);
-    }
-
-    componentWillUnmount() {
-        document.removeEventListener('keypress', this.keypressCallback);
-    }
-
     render() {
         const { movie, onBackClick } = this.props;
-
+        console.log("movie", movie);
         return (
             <div className="movie-view">
-                <div className="movie-poster">
-                    <img src={movie.ImagePath} crossOrigin="anonymous" />
-                </div>
-                <div className="movie-title">
-                    <span className="label">Title: </span>
-                    <span className="value">{movie.Title} </span>
-                </div>
-                <div className="movie-description">
-                    <span className="label">Description: </span>
-                    <span className="value">{movie.Description} </span>
-                </div>
-                <button onClick={() => { onBackClick(null); }}>Back</button>
+                <Card>
+                    <Card.Body>
+                        <Card.Img src={movie.ImagePath} crossOrigin="anonymous" />
+                        <Card.Title className="movie-title"> {movie.Title} </Card.Title>
+                        <Card.Text> {movie.Description} </Card.Text>
+                        <div>
+                            <Link to={`/directors/${movie.Director.Name}`}>
+                                <Button variant="link">Director</Button>
+                            </Link>
+
+                            <Link to={`/genres/${movie.Genre.Name}`}>
+                                <Button variant="link">Genre</Button>
+                            </Link>
+                            <Button
+                                value={movie._id}
+                                onClick={(e) => this.addFavoriteMovie(e, movie)}
+                            >
+                                {" "}
+                                Add to Favorites{" "}
+                            </Button>
+                            {/* <button onClick={() => { onBackClick(null); }}>Back</button> */}
+                            <Button onClick={onBackClick} variant="secondary">
+                                {" "}
+                                Back{" "}
+                            </Button>
+                        </div>
+                    </Card.Body>
+                </Card>
             </div>
         );
     }
